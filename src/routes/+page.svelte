1e0ed6f5048f9b50473ae159d97a2e46fded5684
zhifez <zhifez.lee@gmail.com>
2025-05-16 23:37:15 +0800

    Added first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>Links</h1>
-<p>by zhifei.dev</p>
-<button>Create your own links page now</button>
+<div class="w-screen h-screen flex flex-col gap-4 justify-center items-center">
+  <div class="w-40 h-40 rounded-full bg-blue-500"></div>
+  <div class="flex flex-col gap-1 items-center">
+    <h1 class="text-4xl font-semibold">Links</h1>
+    <p class="text-md font-semibold">by zhifei.dev</p>
+  </div>
+  <button class="px-4 py-2 bg-blue-500 hover:opacity-90 cursor-pointer text-white rounded-full">Create your Links now</button>
+</div>
